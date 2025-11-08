24659afe502036123c2b64e54592560919af4bbe
Topseen-a <abodunrintopseen@gmail.com>
2025-11-08 22:07:59 +0100

    first commit

---
+++ assignment-12-on-reversed-order/ReversedOrder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    public class ReversedOrder{
+        public static void main(String[]args){
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the number to be reversed: ");
+        int number = input.nextInt();
+
+        
+
+        }
+
+
+    }

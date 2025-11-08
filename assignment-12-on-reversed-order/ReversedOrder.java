8a7ff48d4f71a190ca2092cb49a1e59eed5e0d31
Topseen-a <abodunrintopseen@gmail.com>
2025-11-08 22:31:41 +0100

    second commit

--- assignment-12-on-reversed-order/ReversedOrder.java
+++ assignment-12-on-reversed-order/ReversedOrder.java
@@ -7,9 +7,12 @@ import java.util.Scanner;
         System.out.print("Enter the number to be reversed: ");
         int number = input.nextInt();
 
+        int a = (number / 1000);
+        int b = (number / 100) % 10;
+        int c = (number / 10) % 10;
+        int d = number % 10;
         
-
+        int reversedNumber = (d * 1000) + (c * 100) + (b * 10) + a;
+        System.out.println("Reversed number is: " + reversedNumber);
         }
-
-
     }
